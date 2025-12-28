6edc51ce7e66fab77b443296a343f6d0d382e389
Victor.dev <68251707+victordev-Ops@users.noreply.github.com>
2025-12-28 16:50:02 +0100

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -24,7 +24,7 @@ export default async function RootLayout({
 }: {
   children: React.ReactNode;
 }) {
-  const supabase = createSupabaseServerClient;
+  const supabase = createSupabaseServerClient();
 
   const {
     data: { user },
