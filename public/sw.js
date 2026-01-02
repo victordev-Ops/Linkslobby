8b21a49339f68e5108404ccbfd52863a1bf14a06
Victor.dev <68251707+victordev-Ops@users.noreply.github.com>
2026-01-02 15:22:15 +0100

    Create sw.js

---
+++ public/sw.js
@@ -0,0 +1,18 @@
+self.addEventListener('push', function (event) {
+  const data = event.data.json()
+  const options = {
+    body: data.body,
+    icon: '/icons/icon-192.png',
+    badge: '/icons/icon-192.png',
+    data: { url: data.url }
+  }
+
+  event.waitUntil(
+    self.registration.showNotification(data.title, options)
+  )
+})
+
+self.addEventListener('notificationclick', function (event) {
+  event.notification.close()
+  event.waitUntil(clients.openWindow(event.notification.data.url))
+})
