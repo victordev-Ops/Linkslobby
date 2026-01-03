43bc12f2ddc3b01628fbda52b4ddd72def5c2715
Victor.dev <68251707+victordev-Ops@users.noreply.github.com>
2026-01-03 14:54:50 +0100

    Update serwist.config.mjs

--- serwist.config.mjs
+++ serwist.config.mjs
@@ -1,10 +1,7 @@
-import { defaultCache } from "@serwist/next/worker";
-import type { SerwistConfig } from "serwist";
-
 export default {
   swSrc: "src/sw.ts",
   swDest: "public/sw.js",
   globDirectory: "public",
   injectionPoint: "self.__SW_MANIFEST",
-  runtimeCaching: defaultCache,
-} satisfies SerwistConfig;
+  // We will move the runtimeCaching logic into the sw.ts file instead
+};
