d695dca700c50b6ef92ea83ce9f5376239e752dd
Victor.dev <68251707+victordev-Ops@users.noreply.github.com>
2026-01-04 20:07:26 +0100

    Update manifest.ts

--- app/manifest.ts
+++ app/manifest.ts
@@ -11,13 +11,13 @@ export default function manifest(): MetadataRoute.Manifest {
     theme_color: "#000000",
     icons: [
       {
-        src: "/icon-192x192.png",
+        src: "/logo.png",
         sizes: "192x192",
         type: "image/png",
         purpose: "maskable",
       },
       {
-        src: "/icon-512x512.png",
+        src: "/logo.png",
         sizes: "512x512",
         type: "image/png",
       },
