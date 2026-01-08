cebb6cae290c640d2468ee1666ec95c2799d72bf
Victor.dev <68251707+victordev-Ops@users.noreply.github.com>
2026-01-08 17:12:51 +0100

    Update client.ts

--- src/lib/supabase/client.ts
+++ src/lib/supabase/client.ts
@@ -1,8 +1,16 @@
 // src/lib/supabase/client.ts
 import { createBrowserClient } from '@supabase/ssr'
 
-export const createClient = () => 
-  createBrowserClient(
+// Create a variable to hold the singleton instance
+let client: ReturnType<typeof createBrowserClient> | undefined
+
+export const createClient = () => {
+  if (client) return client
+
+  client = createBrowserClient(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
     process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
   )
+  
+  return client
+}
