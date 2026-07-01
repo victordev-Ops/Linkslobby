1986db9aac0a0e3266cdff273ec549d1fd39b0c2
Victor.dev <68251707+victordev-Ops@users.noreply.github.com>
2026-07-01 02:55:57 +0100

    Update RPSWaitingRoom.tsx

--- app/rps/components/RPSWaitingRoom.tsx
+++ app/rps/components/RPSWaitingRoom.tsx
@@ -132,4 +132,5 @@ export function RPSWaitingRoom({ engine }: { engine: ReturnType<typeof useRPSEng
         </div>
     )
                         }
-            
+
+                                
